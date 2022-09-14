a54236f61ab2105de609e4b267bac78564da75f3
Haoxuan Gu <985121841@qq.com>
2022-09-14 18:34:01 +0100

    Initial structure.

--- fundamentals.js
+++ fundamentals.js
@@ -1,1 +1,7 @@
-const aCar = { owner : "Joe Bloggs", type : 'Toyota Corolla 1.8', registration : '201WD1058' };
+const aCar = {
+    owner : "Joe Bloggs",
+    type : 'Toyota Corolla 1.8',
+    registration : '201WD1058'
+  };
+  
+  console.log(aCar.owner);
