6dc13d3de443c5164b8a3af7fdbbbee57b16cfd3
Gu Haoxuan <985121841@qq.com>
2022-09-16 09:38:11 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -4,9 +4,16 @@ const aCar = {
     Make: 'Toyota',
     Model: 'Corolla',
     CC: 1.8,
+    milage: 10000,
+    colour:{
+      exterior: "red",
+      interior: {texture: "leather",shade: "cream"}
+    },
+
   },
   registration : '201WD1058'
 };
 
 console.log(aCar.owner);
-console.log(aCar.owner+'drives a'+aCar.type.Make)
+console.log(aCar.owner+' drives a '+aCar.type.Make)
+console.log("It is a " + aCar.type.colour.exterior+" car, " + aCar.type.milage +" milage, with "  + aCar.type.colour.interior.texture + " interior.")
