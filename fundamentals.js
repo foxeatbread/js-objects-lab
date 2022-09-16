98ded61de05ed86331054824e2b8b0667bff1237
Gu Haoxuan <985121841@qq.com>
2022-09-16 08:54:28 +0100

    Nested objects.

--- fundamentals.js
+++ fundamentals.js
@@ -1,7 +1,12 @@
 const aCar = {
-  owner : "Joe Bloggs",
-  type : 'Toyota Corolla 1.8',
+  owner : 'Joe Bloggs',
+  type :{
+    Make: 'Toyota',
+    Model: 'Corolla',
+    CC: 1.8,
+  },
   registration : '201WD1058'
 };
 
 console.log(aCar.owner);
+console.log(aCar.owner+'drives a'+aCar.type.Make)
