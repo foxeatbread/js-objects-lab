3b473ea15d03b6fe2de7e6e358dd988eab53faf3
Gu Haoxuan <985121841@qq.com>
2022-09-16 10:32:02 +0100

    Loop constructs.

--- fundamentals.js
+++ fundamentals.js
@@ -16,8 +16,16 @@ const aCar = {
     },
 
   },
+  features : ['Parking assist', 'Alarm', 'Tow-bar'],
   registration : '201WD1058'
 };
+for(let i = 0; i < aCar.features.length; i++){
+  console.log(aCar.features[i]);
+}
+for(let i = 0; i < aCar.privious_owners.length; i++){
+  j = i+1
+  console.log('privious owner '+ j + ':' +aCar.privious_owners[i].name)
+}
 
 console.log(aCar.owner);
 console.log(aCar.owner+' drives a '+aCar.type.Make);
